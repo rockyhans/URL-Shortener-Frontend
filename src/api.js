97b5e34137f5592020162d795d0bc76f66deccfb
rockyhans <rdanishrizwan@gmail.com>
2025-08-02 19:45:37 +0530

    Finished

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://devconnect-backend-5sxs.onrender.com/api" });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
